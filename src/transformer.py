2edbb39ff3306bb29c6b765cf55beb12ff666a36
Jpoker01 <jiripokorny455@gmail.com>
2026-09-01 09:31:23 +0000

    src/transformer set-up

--- src/transformer.py
+++ src/transformer.py
@@ -1,1 +1,35 @@
 """Transformer fine-tuning utilities"""
+import numpy as np
+
+import torch
+from torch.utils.data import Dataset
+
+from sklearn.metrics import accuracy_score, f1_score
+from transformers import Trainer
+
+
+class TextClassificationDataset(Dataset):
+    """Pytorch dataset definition for the Trainer"""
+    def __init__(self, texts, labels, tokenizer, max_length):
+        self.texts = list(texts)
+        self.labels = list(labels)
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        encoding = self.tokenizer(
+            self.texts[idx],
+            truncation=True,
+            padding="max_length",
+            max_length=self.max_length,
+            return_tensors="pt",
+        )
+        return {
+            "input_ids": encoding["input_ids"].squeeze(0),
+            "attention_mask": encoding["attention_mask"].squeeze(0),
+            "labels": torch.tensor(self.labels[idx], dtype=torch.long),
+        }
+
